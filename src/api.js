0bc0e5defc7351a8e3b08c4ba9a7f225850c8f7f
sushilnamberdar <sushilnamberdar000@gmail.com>
2025-08-10 15:56:30 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // Email Verification APIs
 // const apiurl = 'http://localhost:5080/api'
-const apiurl = 'https://e-shop-backend-eosin.vercel.app'
+const apiurl = 'https://e-shop-backend-eosin.vercel.app/api'
 
 
 
